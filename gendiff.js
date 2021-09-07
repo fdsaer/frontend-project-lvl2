de0054f5a20b2249223683cf237944935b96f792
Vladimir Stepin <vladimirstyopin@mail.ru>
2021-09-07 16:39:29 +0300

    step 1

---
+++ gendiff.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander/esm.mjs';
+const program = new Command();
+
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
+
+program.parse();
+
+if (!program.args.length) program.help();
