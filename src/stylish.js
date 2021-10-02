869eb3030e768118f9954a54b89faeacff5393a0
Vladimir Stepin <vladimirstyopin@mail.ru>
2021-10-02 18:55:13 +0300

    refactor stylish

--- src/stylish.js
+++ src/stylish.js
@@ -2,41 +2,38 @@ const makeOffset = (filler, number) => (
   new Array(number).fill(filler).join('')
 );
 
-const makeString = ([key, val, status, val2], replacer, offset) => {
+const stringMaker = (formatter, replacer, offset, key) => (val, postReplacer) => (
+  `${postReplacer}${key.toString()}: ${formatter(val, replacer, offset)}`
+);
+
+const parseItem = ([key, val, status, val2], formatter, replacer, offset) => {
   const joinReplacer = `\n${makeOffset(replacer, offset + 1)}`;
   const addedReplacer = `${joinReplacer.slice(0, -2)}+ `;
   const deletedReplacer = `${joinReplacer.slice(0, -2)}- `;
-  let resultString = '';
+  const makeString = stringMaker(formatter, replacer, offset + 1, key);
+  let result = '';
   switch (status) {
-    case ('added'): {
-      resultString = `${addedReplacer}${key.toString()}: ${stylish(val, replacer, offset + 1)}`;
+    case ('added'):
+      result = makeString(val, addedReplacer);
       break;
-    }
-    case ('deleted'): {
-      resultString = `${deletedReplacer}${key.toString()}: ${stylish(val, replacer, offset + 1)}`;
+    case ('deleted'):
+      result = makeString(val, deletedReplacer);
       break;
-    }
-    case ('changed'): {
-      resultString = `${deletedReplacer}${key.toString()}: ${stylish(val, replacer, offset + 1)}`
-        + `${addedReplacer}${key.toString()}: ${stylish(val2, replacer, offset + 1)}`;
+    case ('changed'):
+      result = makeString(val, deletedReplacer)
+        + makeString(val2, addedReplacer);
       break;
-    }
-    case ('equal'): {
-      resultString = `${joinReplacer}${key.toString()}: ${stylish(val, replacer, offset + 1)}`;
-      break;
-    }
-    default: {
-      resultString = `${joinReplacer}${key.toString()}: ${stylish(val, replacer, offset + 1)}`;
-    }
+    default:
+      result = makeString(val, joinReplacer);
   }
-  return resultString;
-}
+  return result;
+};
 
 const stylish = (data, replacer, offset) => {
   if (!Array.isArray(data)) {
     return data === null ? 'null' : data.toString();
   }
-  const arr = data.map((item) => makeString(item, replacer, offset));
+  const arr = data.map((item) => parseItem(item, stylish, replacer, offset));
   return `{${arr.join('')}\n${makeOffset(replacer, offset)}}`;
 };
 
